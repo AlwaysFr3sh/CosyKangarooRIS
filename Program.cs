11af5c463acacf8b3fbca5dc0558212f67f2b7f1
AlwaysFr3sh <shimtucker22187@hotmail.com>
2022-05-17 16:43:30 +1000

    implemented some classes, mostly

--- Program.cs
+++ Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+
+namespace CosyKangaroo {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello, World!");
+
+      Order myOrder = new Order();
+      myOrder.AddItem("penis", 100.6m);
+      myOrder.AddItem("butthole", 69.6m);
+      myOrder.AddItem("vagina", 10.0m);
+      Console.WriteLine(myOrder.OrderTotal());
+      Console.WriteLine(string.Join(" ", myOrder.GetOrderItems()));
+    }
+  }
+
+  class Invoice {
+    public Invoice() {
+
+    }
+  }
+
+  class Receipt {
+    public Receipt() {
+
+    }
+  }
+
+  class Reservation {
+    public Reservation() {
+
+    }
+  }
+
+  class Payment {
+    public Payment() {
+
+    }
+  }
+
+  class PaymentMethod {
+    public PaymentMethod() {
+
+    }
+  }
+
+  class CardDetails : PaymentMethod {
+    public CardDetails() {
+
+    }
+  }
+
+  class Cash : PaymentMethod {
+    public Cash() {
+
+    }
+  }
+}
