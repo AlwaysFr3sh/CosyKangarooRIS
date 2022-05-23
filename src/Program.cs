1bf4f326c115c2b398a1d92e3383cc8126e55b08
Tom Hollo <tomhollo@JohnLeallosiMac.modem>
2022-05-23 13:58:06 +1000

    fixing some stuff

--- src/Program.cs
+++ src/Program.cs
@@ -1,60 +1,10 @@
 ﻿using System.Collections.Generic;
-using CosyKangaroo.classes;
+using CosyKangaroo.Models;
 
 namespace CosyKangaroo {
   class Program {
     static void Main(string[] args) {
       Console.WriteLine("Hello, World!");
-    }
-  }
-
-  class Invoice {
-    public Invoice(string saleID, Order invoiceOrder, Customer invoiceCustomer, Waiter invoiceWaiter) {
-      SaleID = saleID;
-      InvoiceOrder = invoiceOrder;
-      InvoiceCustomer = invoiceCustomer;
-      InvoiceWaiter = invoiceWaiter;
-    }
-    private string SaleID { get; set;}
-    private Order InvoiceOrder { get; set; }
-    private Customer InvoiceCustomer { get; set; }
-    private Waiter InvoiceWaiter { get; set; }
-
-  }
-
-  class Receipt {
-    public Receipt() {
-
-    }
-  }
-
-  class Reservation {
-    public Reservation() {
-
-    }
-  }
-
-  class Payment {
-    public Payment() {
-
-    }
-  }
-
-  class PaymentMethod {
-    public PaymentMethod() {
-
-    }
-  }
-
-  class CardDetails : PaymentMethod {
-    public CardDetails() {
-
-    }
-  }
-
-  class Cash : PaymentMethod {
-    public Cash() {
-
     }
   }
 }
