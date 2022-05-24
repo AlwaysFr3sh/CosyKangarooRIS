8d3c92954fe99fe62b38bb849086cf4a4b854512
Tom Hollo <tomhollo@JohnLeallosiMac.modem>
2022-05-24 11:33:00 +1000

    Semi decent project structure, database working

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,24 @@
 ﻿using System.Collections.Generic;
 using CosyKangaroo.Models;
+using CosyKangaroo.Presentation;
+using CosyKangaroo.Database;
 
 namespace CosyKangaroo {
   class Program {
     static void Main(string[] args) {
-      Console.WriteLine("Hello, World!");
+      // Initialize database
+      DatabaseInterface.OpenDatabaseConnection();
+
+      // testing views
+      RegistrationView registrationView = new RegistrationView("Register");
+      LoginView loginView = new LoginView("Login");
+      LogoutView logoutView = new LogoutView("logout");
+      View[] views = new View[] {registrationView, loginView, logoutView};
+      MainMenu.AddView(views);
+      MainMenu.Display();
+
+      // Close databse
+      DatabaseInterface.CloseDatabaseConnection();
     }
   }
 }
